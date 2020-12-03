4fa4f4a3883cbb6d6d12b294ec71a2cbb82c4968
Cedric P <cedric.pages@le-campus-numerique.fr>
2020-12-03 15:53:04 +0100

    Intégration d'un H1

--- catalogue_1.php
+++ catalogue_1.php
@@ -29,7 +29,7 @@ $list_products = [
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     </head>
     <body>
-
+<h1>Mes articles de ouf</h1>
     <?php
     for ($i = 0; $i < count($list_products); $i++) {
         $list = $list_products[$i];
