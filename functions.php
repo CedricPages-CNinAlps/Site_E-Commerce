33dafbde7f4d16dbc469959e40668b306396e09c
Cedric P <cedric.pages@le-campus-numerique.fr>
2020-12-03 13:39:54 +0100

    Functions start

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+function displayItem1(){
+    "Nom" => "Article_1",
+    "Prix" => 10 . " €" ,
+    "Photo" => "img/images.jpg"
+}
+
+function displayItem2(){
+    "Nom" => "Article_2",
+    "Prix" => 15 . " €",
+    "Photo" => "img/102336.jpg"
+}
+
+function displayItem3(){
+    "Nom" => "Article_3",
+    "Prix" => 20 . " €",
+    "Photo" => "img/Nain_medite.jpg"
+}
+
+function displayItem(){}
