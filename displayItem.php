1822f8780d39f11bc43ddc13b5a5624136dc83e1
Cedric P <cedric.pages@le-campus-numerique.fr>
2020-12-04 11:49:25 +0100

    Etape 5 Mise en place du message d'erreur V1

--- displayItem.php
+++ displayItem.php
@@ -12,9 +12,13 @@
 </head>
 <body>
 <?php
-
+if (!empty($_POST["nom"]) AND !empty($_POST["prix"]) AND $_POST["prix"]>0){
+    echo "Le nom du produit est " . htmlspecialchars($_POST["nom"]) . " et il est au prix de ". htmlspecialchars($_POST["prix"]);
+    }
+else {
+    echo 'Merci de completer tous les champs et/ou de mettre un prix supérieur à 0 !';
+    }
 ?>
-<p>Le nom du produit est "<?= htmlspecialchars($_POST["nom"]); ?>" et il est au prix de "<?= htmlspecialchars($_POST["prix"]); ?>".</p>
 </body>
 
 </html>
